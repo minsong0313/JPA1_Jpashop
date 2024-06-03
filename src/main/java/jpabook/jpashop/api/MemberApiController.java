334a3cbdf16933c85cd9ead58c546b8051e29c1b
minsong0313 <57400913+minsong0313@users.noreply.github.com>
2024-06-03 17:46:32 +0900

    feat: 회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,11 +10,43 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController // => @Controller + @ResponseBody
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    //회원 조회 - v1
+    @GetMapping("/api/v1/members")
+    public List<Member> memberV1() {
+        return memberService.findMembers();
+    }
+
+    //회원 조회 - v2
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     //회원 등록 - v1
     @PostMapping("/api/v1/members")
